bb1d556af58db58efa11b74344ff9c2e8f76832f
Sofia Andersson <sofia.e.i.andersson@gmail.com>
2022-12-03 20:19:41 +0100

    Test 1 including API

---
+++ code/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+import ThoughtList from 'components/ThoughtList';
+import ThoughtForm from 'components/ThoughtForm';
+
+// const API_URL = 'https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/'
+const API_URL = 'https://project-gratitude-journal-ircjrh2jfq-lz.a.run.app/'
+
+export const App = () => {
+  const [thoughtList, setThoughtList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newThought, setNewThought] = useState('');
+
+  const fetchThoughts = () => {
+    setLoading(true);
+    fetch(`${API_URL}thoughts`)
+      .then((res) => res.json())
+      .then((data) => setThoughtList(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    fetchThoughts();
+  }, []);
+
+  const handleNewLikeChange = (_id) => {
+    const options = { method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      } }
+    fetch(`${API_URL}thoughts/${_id}/like`, options)
+      .then((res) => res.json())
+      .then(() => fetchThoughts())
+  }
+
+  const handleNewThoughtChange = (event) => {
+    setNewThought(event.target.value)
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: newThought
+      })
+    }
+
+    fetch(`${API_URL}thoughts`, options)
+      .then((res) => res.json())
+      .then(() => fetchThoughts())
+      .finally(() => setNewThought(''));
+    // .catch((err) => console.error(err));
+  }
+  return (
+    <div>
+      <ThoughtForm
+        newThought={newThought}
+        onNewThoughtChange={handleNewThoughtChange}
+        onFormSubmit={onFormSubmit} />
+      <ThoughtList
+        loading={loading}
+        thoughtList={thoughtList}
+        setThoughtList={setThoughtList}
+        handleNewLikeChange={handleNewLikeChange} />
+    </div>
+  );
+}
---
+++ code/src/components/ThoughtList.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import { formatDistance } from 'date-fns';
+
+const ThoughtList = ({ loading, thoughtList, handleNewLikeChange }) => {
+  if (loading) {
+    return <h1>Loading in progress...</h1>
+  }
+  return (
+    <section>
+      {thoughtList.map((thought) => {
+        console.log('thought', thought)
+        return (
+        // eslint-disable-next-line no-underscore-dangle
+          <div className="message-wrapper" key={thought._id}>
+            <h4 className="thought-message">{thought.message}</h4>
+            <div className="heart-counter-wrapper">
+              <div className="heart-details">
+                <button
+                  type="submit"
+                  className={thought.hearts === 0 ? 'no-likes-button' : 'has-likes-button'}
+                  onClick={() => handleNewLikeChange(thought._id)}>❤️
+                </button>
+                <p className="heart-count"> x {thought.hearts}</p>
+              </div>
+              <p className="timestamp">{formatDistance(new Date(thought.createdAt), Date.now(), {
+                addSuffix: true
+              })}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+export default ThoughtList;
